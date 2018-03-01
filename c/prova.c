fa7291f42b145791be8c93e340370b29a86f2c98
Mauro Bisiani <lain@vmi145328.contaboserver.net>
2018-03-01 14:15:06 +0100

    added some preprocessor lines

--- c/prova.c
+++ c/prova.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
 
 int main(int argc, char **argv) {
   printf("Lain Iwakura\n");
