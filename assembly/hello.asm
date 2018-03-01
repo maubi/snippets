3f6ad5a1b3e5ac0cdd1b96ba98368ac2261683bb
Mauro Bisiani <lain@vmi145328.contaboserver.net>
2018-03-01 18:01:14 +0100

    Added few comments

--- assembly/hello.asm
+++ assembly/hello.asm
@@ -1,15 +1,15 @@
 section     .text
-    global  _start ; must be declared for linker
-_start:             ; entry point
-    mov edx,len
-    mov ecx,msg
-    mov ebx,1
-    mov eax,4
-    int 0x80
+    global  _start 	; must be declared for linker
+_start:            	; tells linker entry point
+    mov edx,len		; message length
+    mov ecx,msg		; message to write
+    mov ebx,1		; file descriptor (stdout)
+    mov eax,4		; system call number (sys_write)
+    int 0x80		; call kernel
 
-    mov eax,1
-    int 0x80
+    mov eax,1		; system call number (sys_exit)
+    int 0x80		; call kernel
 
 section     .data
-msg db 'Hello, World!', 0xa
-len equ $ - msg
+msg db 'Hello, World!', 0xa		; our dear string
+len equ $ - msg					; length of our dear string
