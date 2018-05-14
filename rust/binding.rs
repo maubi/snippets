952ea4aca2696cffd47121f538fa12b879bb4aec
Mauro Bisiani <lain@vmi145328.contaboserver.net>
2018-05-14 15:23:44 +0200

    Added Rust snippets

---
+++ rust/binding.rs
@@ -0,0 +1,13 @@
+fn main () {
+	let x = 5;
+	let (y, z) = (1, 2);
+	let w: i32 = 10;
+	let string1 = "Hachiko";
+
+	let mut a: i32 = 1;
+	a = 10;
+
+	println!("The value of a is: {}", a);
+	println!("{} {} {}", x, y, z);
+	println!("{}", string1);
+}
