3d039d858c8144c8157690d8a84e36ee6e488f45
Mauro Bisiani <lain@vmi145328.contaboserver.net>
2018-09-12 17:32:29 +0200

    Added proxy pattern snippet (C++)

---
+++ c++/proxy2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class ICar {
+public:
+	virtual void driveCar() = 0;
+};
+
+class Car: public ICar {
+public:
+	virtual void driveCar() override {
+		cout << "Car has been driven!" << endl;
+	}
+};
+
+class ProxyCar: public ICar {
+private:
+	int _driverAge;
+	ICar *realCar;
+public:
+	ProxyCar(int age) {
+		_driverAge = age;
+		realCar = new Car;
+	}
+
+	virtual void driveCar() override {
+		if (_driverAge < 18) {
+			cout << "Driver is underage. Cannot drive car!" << endl;
+		} else {
+			realCar->driveCar();
+		}
+	}
+};
+
+int main(void) {
+	ICar *myCar = new ProxyCar(15);
+	myCar->driveCar();
+
+	ICar *myCar2 = new ProxyCar(20);
+	myCar2->driveCar();
+	return 0;
+}
+
+
+
+
+
+
