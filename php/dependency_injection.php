ca6b4117723af24b7e3edf34c960e5f27b92461f
Mauro Bisiani <mauro.bisiani@beta80group.it>
2019-01-17 18:00:28 +0100

    Added 1 OOP PHP script (DI example)

---
+++ php/dependency_injection.php
@@ -0,0 +1,41 @@
+<?php
+
+class Anime {
+	private $title;
+	private $lead;
+
+	public function __construct($title, Person $lead) {
+		$this->title = $title;
+		$this->lead = $lead;
+	}
+
+	public function leadQuote() {
+		$this->lead->speak();
+	}
+}
+
+// service base
+abstract class Person {
+	public abstract function speak();
+}
+
+// service implementations
+class MadScientist extends Person {
+	public function speak() {
+		echo "I'm mad scientist. It's so cool. Sonofabitch!\n";
+	}
+}
+
+class Otaku extends Person {
+	public function speak() {
+		echo "Sugoi!\n";
+	}
+}
+
+// client code
+$saekano = new Anime('Saekano', new Otaku);
+$saekano->leadQuote();
+
+$steins = new Anime('Steins;Gate', new MadScientist);
+$steins->leadQuote();
+?>
