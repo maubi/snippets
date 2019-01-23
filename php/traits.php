e5ec650dc39054c93b879c3b30f5a1fa11b6861b
Mauro Bisiani <mauro.bisiani@beta80group.it>
2019-01-23 10:45:06 +0100

    Added an example of properties use in traits

--- php/traits.php
+++ php/traits.php
@@ -39,4 +39,52 @@ $greet1->egg();
 $greet2 = new AnotherGreet;
 $greet2->greet();		// TRAIT overrides INHERITED
 $greet2->egg();
+
+
+trait DatiAnagrafici {
+	public $first_name;
+	public $last_name;
+	public $age;
+	public $sex;
+	public $address1;
+	public $address2;
+
+	public abstract function speak();
+}
+
+abstract class Person {
+	use DatiAnagrafici;
+}
+
+class Otaku extends Person {
+	public function speak() {
+		echo $this->first_name, ": Konbanwa!\n";
+	}
+}
+
+class Normie extends Person {
+	public function speak() {
+		echo $this->first_name, ": Yo!\n";	
+	}
+}
+
+$harunobu = new Otaku;
+$harunobu->first_name = 'Harunobu';
+$harunobu->last_name = 'Madarame';
+$harunobu->speak();
+
+$saki = new Normie;
+$saki->first_name = 'Saki';
+$saki->last_name = 'Kasukabe';
+$saki->speak();
+
 ?>
+
+
+
+
+
+
+
+
+
