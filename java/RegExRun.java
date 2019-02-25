65637271124786042389428d281801a832bad214
Mauro Bisiani <bisianimauro@gmail.com>
2019-02-25 11:52:24 +0100

    Aggiunto 1 snippet per test RegEx in Java

---
+++ java/RegExRun.java
@@ -0,0 +1,20 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegExRun {
+	public static void main(String[] args) {
+		String waifus = "Kaya;Ami;Ami;Lain";
+		String pattern = "Ami";
+		Integer count = 0;
+
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(waifus);
+		while(m.find()) {
+			count++;
+			System.out.printf("MATCH %d\n", count);
+			System.out.printf("START: %d\n", m.start());
+			System.out.printf("END:   %d\n", m.end());
+			System.out.println();
+		}
+	}
+}
