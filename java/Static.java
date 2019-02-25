a4893e544ac41f0ae187f7944fd71d5cadc00e94
Mauro Bisiani <bisianimauro@gmail.com>
2019-02-25 11:54:28 +0100

    Aggiunti snippet per test metodi statici in Java

---
+++ java/Static.java
@@ -0,0 +1,11 @@
+public class Static {
+	public static void eat() {
+		System.out.println("I ATE.");
+	}
+	public static void sleep() {
+		System.out.println("I SLEPT.");
+	}
+	public static void walk() {
+		System.out.println("I WALKED.");
+	}
+}
---
+++ java/StaticRun.java
@@ -0,0 +1,7 @@
+public class StaticRun {
+	public static void main(String[] args) {
+		Static.eat();
+		Static.sleep();
+		Static.walk();
+	}
+}
