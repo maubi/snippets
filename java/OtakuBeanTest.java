f0753360e35edfb141c78f19ae2b3b00b27de8f7
Mauro Bisiani <bisianimauro@gmail.com>
2019-02-28 16:56:37 +0100

    Minor add

--- java/OtakuBeanTest.java
+++ java/OtakuBeanTest.java
@@ -10,5 +10,14 @@ public class OtakuBeanTest {
 		System.out.println(madarame.getName());
 		System.out.println(madarame.getFavAnime());
 		System.out.println(madarame.getFavManga());
+
+		OtakuBean sasahara = new OtakuBean();
+		sasahara.setName("Sasahara Kenji");
+		sasahara.setFavAnime("Kujibiki Unbalance");
+		sasahara.setFavManga("Rin");
+
+		System.out.println(sasahara.getName());
+		System.out.println(sasahara.getFavAnime());
+		System.out.println(sasahara.getFavManga());
 	}
 }
