20e085498d3e5ff50126c965100b3f154bd84b34
Mauro Bisiani <bisianimauro@gmail.com>
2019-03-08 11:13:04 +0100

    Added a second instance of a different type

--- java/GenericClassTest.java
+++ java/GenericClassTest.java
@@ -10,10 +10,14 @@ public class GenericClassTest <E> {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("MAIN");
-
 		GenericClassTest<String> obj1 = new GenericClassTest<String>();
 		obj1.setVar("Tomoko is love. Tomoko is life.");
-		System.out.println(obj1.getVar());
+		System.out.println("obj1.val = " + obj1.getVar());
+
+		System.out.println();
+
+		GenericClassTest<Integer> obj2 = new GenericClassTest<Integer>();
+		obj2.setVar(1978);
+		System.out.println("obj2.val = " + obj2.getVar());
 	}
 }
